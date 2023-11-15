9892d6dbcf8999ae88612817e2b7b5b348b970db
SebastianLozanoValle <90271134+SebastianLozanoValle@users.noreply.github.com>
2023-11-15 03:41:13 -0500

    primer commit lambda
    se inicializo el proyecto, aprendiendo go y aws : )

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"context"
+
+	"github.com/SebastianLozanoValle/gambituser/awsgo"
+	events "github.com/aws/aws-lambda-go/events"
+	lambda "github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(EjecutoLambda)
+}
+
+func EjecutoLambda(ctx context.Context, event events.CognitoEventUserPoolsPostConfirmation) (events.CognitoEventUserPoolsPostConfirmation, error) {
+	awsgo.InicializoAWS()
+}
